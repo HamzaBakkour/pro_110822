6b80495e397be3fba7dab71a40715988eef6042a
HamzaBakkour <alexander.x4.hb@outlook.com>
2023-04-19 19:09:20 +0200

    senduserinput test

---
+++ tests/test_senduserinput.py
@@ -0,0 +1,14 @@
+import unittest
+from pro_110822.server.senduserinput import SendUserInput
+
+#start the listener
+#moch the add to queue
+#   add to list instead
+
+class TestSendUserInput(unittest.TestCase):
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+
+    def test_hello(self):
+        raise NotImplementedError
+
